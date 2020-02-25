dd8cc1c84bb26b57766a30807f82731a4e96a300
Kenn3Th <Kenneth.Eikrehagen@gmail.com>
2020-02-25 13:01:41 +0100

    Legger til filer fra MAC

---
+++ robotArm_servoReset/robotArm_servoReset.ino
@@ -0,0 +1,20 @@
+int servopin = A0;
+int myangle;
+int pulsewidth;
+
+void setup() {
+ pinMode(servopin,OUTPUT); //Set steering gear interface as output
+}
+
+void servopulse(int servopin, int myangle) {
+  pulsewidth = (myangle*11) + 500; //Converts the angle to pulsewidth value 500-2480
+  digitalWrite(servopin, HIGH);
+  delayMicroseconds(pulsewidth);
+  digitalWrite(servopin, LOW);
+  delay(20-(pulsewidth/1000));  
+}
+
+void loop() {
+  servopulse(servopin,0); //set steering gear angle
+
+}
